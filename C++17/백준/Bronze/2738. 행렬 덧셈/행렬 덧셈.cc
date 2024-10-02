5a342522c26529345b67c3e5b7252d0c8e444a45
nhyun199 <147118363+nhyun199@users.noreply.github.com>
2024-10-02 18:02:22 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    int A[100][100];
+    int B[100][100];
+    cin >> N >> M;
+    
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
+            cin >> A[i][j];
+        
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
+            cin >> B[i][j];
+    
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            cout << A[i][j] + B[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
