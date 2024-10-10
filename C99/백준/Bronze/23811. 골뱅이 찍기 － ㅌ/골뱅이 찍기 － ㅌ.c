01abe22c1238185a905e6e2c366445619595a511
nhyun199 <147118363+nhyun199@users.noreply.github.com>
2024-10-10 09:21:19 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+	scanf("%d", &n);
+
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			printf("@@@@@");
+		}
+		printf("\n");
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("@");
+		}
+		printf("\n");
+	}
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			printf("@@@@@");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("@");
+		}
+		printf("\n");
+	}
+
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			printf("@@@@@");
+		}
+		printf("\n");
+	}
+}
