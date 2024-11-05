3c7bb874139cc36ad2b3bfb966571d16173ad406
nhyun199 <147118363+nhyun199@users.noreply.github.com>
2024-11-06 08:53:12 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/13985. Equality/Equality.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    char d, e;
+    scanf("%d %c %d %c %d", &a, &d, &b, &e, &c);
+    if(a + b == c)
+        printf("YES");
+    else
+        printf("NO");
+}
